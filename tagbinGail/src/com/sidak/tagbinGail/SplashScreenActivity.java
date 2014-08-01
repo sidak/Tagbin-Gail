73310b564bfc44975c32b21990cbde9e0b714cb8
sidak <spssachdeva21695@gmail.com>
2014-08-01 14:54:10 +0530

    add splashscreen in landscape mode

---
+++ tagbinGail/src/com/sidak/tagbinGail/SplashScreenActivity.java
@@ -0,0 +1,27 @@
+package com.sidak.tagbinGail;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends Activity {
+	
+	private static int SPLASH_TIME_OUT = 1500;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+		
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
+	}
+}
