97e7b02b1d86e2164c27d17f348f583a4fe146d0
sidak <spssachdeva21695@gmail.com>
2014-08-02 17:01:03 +0530

    use linearlayout completely in activity_industry.xml

--- tagbinGail/src/com/sidak/tagbinGail/MainActivity.java
+++ tagbinGail/src/com/sidak/tagbinGail/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sidak.tagbinGail;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,5 +12,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
+		Intent intent= new Intent(this, IndustryActivity.class);
+		startActivity(intent);
+}
 }
