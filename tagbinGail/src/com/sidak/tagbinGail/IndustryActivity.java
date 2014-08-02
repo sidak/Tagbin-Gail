e1643ef0f5c503639b141550dc07dd288f8535f9
sidak <spssachdeva21695@gmail.com>
2014-08-02 13:35:59 +0530

    add layout files for industrial and material activity

---
+++ tagbinGail/src/com/sidak/tagbinGail/IndustryActivity.java
@@ -0,0 +1,15 @@
+package com.sidak.tagbinGail;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class IndustryActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_industry);
+	}
+}
