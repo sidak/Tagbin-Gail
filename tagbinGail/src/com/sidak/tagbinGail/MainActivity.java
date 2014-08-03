306a2bcdef6c8b8780318471ef738c4933cd30eb
sidak <spssachdeva21695@gmail.com>
2014-08-03 12:33:13 +0530

    update method of captyring photos

--- tagbinGail/src/com/sidak/tagbinGail/MainActivity.java
+++ tagbinGail/src/com/sidak/tagbinGail/MainActivity.java
@@ -1,16 +1,18 @@
 package com.sidak.tagbinGail;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,81 +22,122 @@ public class MainActivity extends Activity {
 	private Button capture_pic;
 	String mCurrentPhotoPath;
 	static final int REQUEST_TAKE_PHOTO = 1;
+	private Uri fileUri; // file url to store image/video
+	public static final int MEDIA_TYPE_IMAGE = 1;
 
+	// directory name to store captured images and videos
+	private static final String IMAGE_DIRECTORY_NAME = "GAIL India Ltd.";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		next=(Button)findViewById(R.id.next_label);
+		next = (Button) findViewById(R.id.next_label);
 		next.setOnClickListener(new View.OnClickListener() {
-		
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent intent= new Intent(MainActivity.this, IndustryActivity.class);
+				Intent intent = new Intent(MainActivity.this,
+						IndustryActivity.class);
 				startActivity(intent);
 			}
 		});
-		capture_pic=(Button)findViewById(R.id.click_pic_label);
+		capture_pic = (Button) findViewById(R.id.click_pic_label);
 		capture_pic.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				dispatchTakePictureIntent();
+				captureImage();
 			}
 		});
-		
+
 	}
 
-	private File createImageFile() throws IOException {
-	    // Create an image file name
-	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-	    String imageFileName = "JPEG_" + timeStamp + "_";
-	    File storageDir = Environment.getExternalStoragePublicDirectory(
-	            Environment.DIRECTORY_PICTURES);
-	    File image = File.createTempFile(
-	        imageFileName,  /* prefix */
-	        ".jpg",         /* suffix */
-	        storageDir      /* directory */
-	    );
-
-	    // Save a file: path for use with ACTION_VIEW intents
-	    mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-	    return image;
-	
+	/**
+	 * Checking device has camera hardware or not
+	 * */
+	private boolean isDeviceSupportCamera() {
+		if (getApplicationContext().getPackageManager().hasSystemFeature(
+				PackageManager.FEATURE_CAMERA)) {
+			// this device has a camera
+			return true;
+		} else {
+			// no camera on this device
+			return false;
+		}
 	}
-	
 
-	private void dispatchTakePictureIntent() {
-	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	    // Ensure that there's a camera activity to handle the intent
-	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-	        // Create the File where the photo should go
-	        File photoFile = null;
-	        try {
-	            photoFile = createImageFile();
-	        } catch (IOException ex) {
-	            // Error occurred while creating the File
-	            ex.printStackTrace();
-	        }
-	        // Continue only if the File was successfully created
-	        if (photoFile != null) {
-	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-	                    Uri.fromFile(photoFile));
-	            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-	        }
-	    }
-	    else{
-	    	Toast.makeText(MainActivity.this, getResources().getString(R.string.no_camera), Toast.LENGTH_LONG).show();
-	    }
+	/**
+	 * Capturing Camera Image will lauch camera app requrest image capture
+	 */
+	private void captureImage() {
+		if (!isDeviceSupportCamera()) {
+			Toast.makeText(MainActivity.this,
+					getResources().getString(R.string.no_camera),
+					Toast.LENGTH_LONG).show();
+
+		} else {
+			Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+			
+			fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
+
+			intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+			// start the image capture Intent
+			startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+		}
 	}
+
+	/**
+	 * Creating file uri to store image/video
+	 */
+	public Uri getOutputMediaFileUri(int type) {
+		return Uri.fromFile(getOutputMediaFile(type));
+	}
+
+	/**
+	 * returning image / video
+	 */
+	private static File getOutputMediaFile(int type) {
+
+		// External sdcard location
+		File mediaStorageDir = new File(
+				Environment
+						.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+				IMAGE_DIRECTORY_NAME);
+
+		// Create the storage directory if it does not exist
+		if (!mediaStorageDir.exists()) {
+			if (!mediaStorageDir.mkdirs()) {
+				Log.d(IMAGE_DIRECTORY_NAME, "Oops! Failed create "
+						+ IMAGE_DIRECTORY_NAME + " directory");
+				return null;
+			}
+		}
+
+		// Create a media file name
+		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+				Locale.getDefault()).format(new Date());
+		File mediaFile;
+		if (type == MEDIA_TYPE_IMAGE) {
+			mediaFile = new File(mediaStorageDir.getPath() + File.separator
+					+ "IMG_" + timeStamp + ".jpg");
+		} else {
+			return null;
+		}
+
+		return mediaFile;
+	}
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-	    		Toast.makeText(MainActivity.this, getResources().getString(R.string.photo_clicked)+" "+mCurrentPhotoPath, Toast.LENGTH_SHORT).show();
-	    }
+		if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+			Toast.makeText(
+					MainActivity.this,
+					getResources().getString(R.string.photo_clicked), Toast.LENGTH_SHORT).show();
+		}
 	}
-	
+
 }
