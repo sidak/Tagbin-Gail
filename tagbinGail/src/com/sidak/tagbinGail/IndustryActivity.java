13bc08992c1909e54aeb0b32fc522ec6938963fd
sidak <spssachdeva21695@gmail.com>
2014-08-03 08:42:45 +0530

    add consumption activity

--- tagbinGail/src/com/sidak/tagbinGail/IndustryActivity.java
+++ tagbinGail/src/com/sidak/tagbinGail/IndustryActivity.java
@@ -1,15 +1,10 @@
 package com.sidak.tagbinGail;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 public class IndustryActivity extends Activity {
 	private Button next;
--- tagbinGail/src/com/sidak/tagbinGail/MaterialActivity.java
+++ tagbinGail/src/com/sidak/tagbinGail/MaterialActivity.java
@@ -1,14 +1,28 @@
 package com.sidak.tagbinGail;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MaterialActivity extends Activity {
-	
+	private Button next;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_material);
+		next=(Button)findViewById(R.id.next_label);
+		next.setOnClickListener(new View.OnClickListener() {
+		
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent= new Intent(MaterialActivity.this, ConsumptionActivity.class);
+				startActivity(intent);
+			}
+		});
 		
 	}
 	
