857ccab904b94fc51eaab4126b4ab2ead12155e6
sidak <spssachdeva21695@gmail.com>
2014-08-03 09:50:49 +0530

    make intent for clicking photos

--- tagbinGail/src/com/sidak/tagbinGail/MainActivity.java
+++ tagbinGail/src/com/sidak/tagbinGail/MainActivity.java
@@ -1,14 +1,27 @@
 package com.sidak.tagbinGail;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private Button next;
-	
+	private Button capture_pic;
+	String mCurrentPhotoPath;
+	static final int REQUEST_TAKE_PHOTO = 1;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,7 +36,65 @@ public class MainActivity extends Activity {
 				startActivity(intent);
 			}
 		});
+		capture_pic=(Button)findViewById(R.id.click_pic_label);
+		capture_pic.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				dispatchTakePictureIntent();
+			}
+		});
 		
+	}
+
+	private File createImageFile() throws IOException {
+	    // Create an image file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = "JPEG_" + timeStamp + "_";
+	    File storageDir = Environment.getExternalStoragePublicDirectory(
+	            Environment.DIRECTORY_PICTURES);
+	    File image = File.createTempFile(
+	        imageFileName,  /* prefix */
+	        ".jpg",         /* suffix */
+	        storageDir      /* directory */
+	    );
+
+	    // Save a file: path for use with ACTION_VIEW intents
+	    mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+	    return image;
+	
+	}
+	
+
+	private void dispatchTakePictureIntent() {
+	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    // Ensure that there's a camera activity to handle the intent
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        // Create the File where the photo should go
+	        File photoFile = null;
+	        try {
+	            photoFile = createImageFile();
+	        } catch (IOException ex) {
+	            // Error occurred while creating the File
+	            ex.printStackTrace();
+	        }
+	        // Continue only if the File was successfully created
+	        if (photoFile != null) {
+	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+	                    Uri.fromFile(photoFile));
+	            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+	        }
+	    }
+	    else{
+	    	Toast.makeText(MainActivity.this, getResources().getString(R.string.no_camera), Toast.LENGTH_LONG).show();
+	    }
+	}
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+	    		Toast.makeText(MainActivity.this, getResources().getString(R.string.photo_clicked)+" "+mCurrentPhotoPath, Toast.LENGTH_SHORT).show();
+	    }
 	}
 	
 }
